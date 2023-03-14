25104909529c073e1f5394c20b99623b6dbedd8f
tevans053194 <tevans053194@gmail.com>
2023-03-14 18:02:59 -0500

    new push

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@
 
 console.log("Hello world!");
 
+function down1() {
+    document.getElementById("myWork").scrollIntoView();
+  }
+
+
+function down2() {
+    document.getElementById("mySkills").scrollIntoView();
+  }
+
+
+function down3() {
+    document.getElementById("myContact").scrollIntoView();
+  }
